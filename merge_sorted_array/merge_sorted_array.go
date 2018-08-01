c211f3decdba9ffc120ad7cc974c615d45528008
cdsurfer0212 <cdsurfer0212@gmail.com>
2018-08-01 11:09:32 +0800

    add Merge Sorted Array

---
+++ merge_sorted_array/merge_sorted_array.go
@@ -0,0 +1,17 @@
+package mergesortedarray
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	current := len(nums1) - 1
+	i := m - 1
+	j := n - 1
+	for i >= 0 || j >= 0 {
+		if j < 0 || (i >= 0 && nums1[i] >= nums2[j]) {
+			nums1[current] = nums1[i]
+			i--
+		} else {
+			nums1[current] = nums2[j]
+			j--
+		}
+		current--
+	}
+}
